3c6be404002dfe4976272b58ed2c00b739137e9a
Amongus37 <105140625+amongusgamer37@users.noreply.github.com>
2022-05-10 23:39:38 +0000

    Added remove role, add role, kick and ban commands!!!!

--- index.js
+++ index.js
@@ -6,12 +6,116 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+let lolcounter = [
+  
+]
+
+
+function updateCounter() {
+    lolcounter++
+  }
+   
+  
 client.on('message', msg => {
+ //add role-delete
+    if (msg.content.toLowerCase().startsWith('m:addrole')) {
+    var sender = msg.member.hasPermission('ADMINISTRATOR')
+    const user = msg.mentions.users.first();
+    const member = msg.guild.member(user);
+    var role = msg.content.split(' ')
+    var mod = '935262106041876550'  
+    var jrmod = '961824021912105050'
+    var event = '961824035199668244'
+    var ver = '935258587486380083'
+      if (Number.isInteger(Number(role[2])) === true){
+      var adding = role[2]
+      }else{
+       switch (role[2]){
+         case 'mod':
+         var adding = mod
+           break;
+         case 'jrmod':
+           var adding = jrmod
+           break;
+         case 'event':
+           var adding = event
+           break;
+         case 'verified', 'ver':
+           var adding = ver
+           break;
+       }
+      }
+   if (sender === true) {
+       member.addRole(adding)
+  .then(msg.channel.send(`Role added to '${member.id}(${member.user.tag})' `))
+  .catch(console.error);
+
+   }else{
+  msg.reply('You don\'t have the perms, loser')
+   }
+  }
+
+
+  if (msg.content.toLowerCase().startsWith('m:removerole')) {
+    var sender = msg.member.hasPermission('ADMINISTRATOR')
+    const user = msg.mentions.users.first();
+    const member = msg.guild.member(user);
+    var role = msg.content.split(' ')
+       var mod = '935262106041876550'  
+    var jrmod = '961824021912105050'
+    var event = '961824035199668244'
+      if (Number.isInteger(Number(role[2])) === true){
+      var adding = role[2]
+      }else{
+       switch (role[2]){
+         case 'mod':
+         var adding = mod
+           break;
+         case 'jrmod':
+           var adding = jrmod
+           break;
+         case 'event':
+           var adding = event
+           break;
+       }
+      }
+   if (sender === true) {
+       member.removeRole(adding)
+  .then(msg.channel.send(`Role removed from '${member.id}(${member.user.tag})'  !`))
+  .catch(console.error);
+
+   }else{
+  msg.reply('You don\'t have the perms, loser')
+   }
+  }
+//role ends
+  
+// lol counter(broken)
+  if (msg.content.toLowerCase() === 'lol') {
+    updateCounter()
+  }
+   
+  
+if (msg.content.toLowerCase() === 'm:lolcounter') {
+    msg.channel.send(`Times 'lol' has been said in Moose Clothing Ltd. since May 7th: \n ${lolcounter} `)
+  
+  }
+//lol counter ends
+if (msg.content.toLowerCase() === 'm:befriend') {
+  var writer = msg.author
+  //msg.author.addFriend();
+  msg.channel.send(' ```command disabled by owner. Reason: \ncrashes bot```');
+  }
+
+  
   if (msg.content.toLowerCase() === 'moose') {
-    msg.channel.send('MOOSE!!');
+    msg.channel.send('MOOSE!!!');
   }
   if (msg.content.toLowerCase() === 'moose cmds') {
-    msg.channel.send("```Moose Bot COMMANDS: \n m:ping: bot replies with pong \n moose: bot says moose \n m:joindate [mention a user]: replies with the day that user joined the server \n m:serverImage: Replies with the server's profile image \n m:pfp [mention a user]: Sends the mentioned user's pfp \n  \nCommands are not case-sensitive, More coming soon!```");
+    msg.channel.send('```Moose Bot COMMANDS: \nm:ping: bot replies with pong \nmoose: bot says moose \nm:joindate [mention a user]: replies with the day that user joined the server \nm:serverImage: Replies with the server\'s profile image \nm:pfp [mention a user]: Sends the mentioned user\'s pfp \n  \nCommands are not case-sensitive, More coming soon! \n \n \n \nSay "mod cmds" for commands made specially for mods!```');
+  }
+  if (msg.content.toLowerCase() === 'mod cmds') {
+    msg.channel.send('```Mod Commands(Must have ADMINISTRATOR permission to use!): \nm:addrole [mention user] [role id]: adds the role to the mentioned user \nm:removerole [mention user] [role id]: opposite of the add~role command \nm:kick [mention user]: kicks the mentioned user from the server \nm:ban [mention user]: bans the mentioned user \n \nRole ID can be substituted for "mod", "jrmod", or "event" which will assign the mentioned user that role.```');
   }
     if (msg.content.toLowerCase() === 'm:ping') {
     msg.channel.send('pong');
@@ -25,7 +129,6 @@ client.on('message', msg => {
   }
   if (msg.content.toLowerCase().startsWith('m:pfp')) {
     const user = msg.mentions.users.first();
-    const member = msg.guild.member(user);
     const pfp = user.avatarURL
  
     msg.channel.send(`${pfp}`);
@@ -43,6 +146,33 @@ msg.channel.send(`https://cdn.discordapp.com/attachments/934607737403351079/9417
     //change the string below into the variable embed when needed
       msg.channel.send('this command is currently disabled');
   }
+
+if (msg.content.toLowerCase().startsWith('m:kick')) {
+    var sender = msg.member.hasPermission('ADMINISTRATOR')
+    const user = msg.mentions.users.first();
+    const member = msg.guild.member(user);
+   if (sender === true) {
+       member.kick(`m:kick run by run by [${msg.member.nickname}]  !`)
+  .then(msg.channel.send(`'${user.tag}'  has been kicked from the server!!! \n YEET! `))
+  .catch(console.error);
+
+   }else{
+  msg.reply('You don\'t have the perms, loser')
+   }
+  }
+if (msg.content.toLowerCase().startsWith('m:ban')) {
+    var sender = msg.member.hasPermission('ADMINISTRATOR')
+    const user = msg.mentions.users.first();
+    const member = msg.guild.member(user);
+   if (sender === true) {
+       member.ban(`m:ban run by [${msg.member.nickname}]  !`)
+  .then(msg.channel.send(`'${user.tag}'  has been banned from the server!!! \n YEET! `))
+  .catch(console.error);
+
+   }else{
+  msg.reply('You don\'t have the perms, loser')
+   }
+  }
 }); 
 
 
@@ -50,7 +180,7 @@ msg.channel.send(`https://cdn.discordapp.com/attachments/934607737403351079/9417
 
 
 var rules = `***NO*** **advertising**
-Please do not advertise in any channels except for #『🪐』advertising
+Please do not advertise in any channels except for #『🪐』advertisng
 
 
 ***NO*** **massive arguments**
@@ -73,16 +203,8 @@ Leaking designs without permission from MerelyMoose#9833 may result in punishmen
 
 `
 
-/*   WIP, add role command
-   if (msg.content.toLowerCase().startsWith === 'm:addrole') {
-   if (msg.author ) {
-    msg.member.addRole('193654001089118208')
-  .then(console.log)
-  .catch(console.error);
-   }else{
-  msg.reply('You don\'t have the perms, loser')
-   }
-  }*/
+
+
 const mySecret = process.env['TOKEN']
 keepAlive()
 client.login(mySecret);
